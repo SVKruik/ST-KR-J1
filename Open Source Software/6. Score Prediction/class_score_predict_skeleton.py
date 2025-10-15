90646accec38b87c75f4a5e0a4a3611fd5ca631e
Stefan Kruik <s.kruik@icloud.com>
2025-10-15 15:44:32 +0900

    Score prediction

---
+++ Open Source Software/6. Score Prediction/class_score_predict_skeleton.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    midterm_range = np.array([0, 125])
+    final_range = np.array([0, 100])
+
+    # Load score data
+    class_kr = np.loadtxt('data/class_score_kr.csv', delimiter=',')
+    class_en = np.loadtxt('data/class_score_en.csv', delimiter=',')
+    data = np.vstack((class_kr, class_en))
+
+    # Estimate a line: final = slope * midterm + y_intercept
+    x = data[:, 0] # midterm scores
+    y = data[:, 1] # final scores
+    n = len(x)
+    x_mean = np.mean(x) # mean of midterm scores
+    y_mean = np.mean(y) # mean of final scores
+    Sxy = np.sum((x - x_mean) * (y - y_mean)) # deviation product
+    Sxx = np.sum((x - x_mean) ** 2) # variance
+    slope = Sxy / Sxx
+    y_intercept = y_mean - slope * x_mean
+    line = [slope, y_intercept]
+
+    # Predict scores
+    final = lambda midterm: line[0] * midterm + line[1]
+    while True:
+        try:
+            given = input('Q) Please input your midterm score (Enter or -1: exit)? ')
+            if given == '' or float(given) < 0:
+                break
+            print(f'A) Your final score is expected to be {final(float(given)):.3f}.')
+        except Exception as ex:
+            print(f'Cannot answer the question. (message: {ex})')
+            break
+
+    # Plot scores and the estimated line
+    plt.figure()
+    plt.plot(data[:,0], data[:,1], 'r.', label='The given data')
+    plt.plot(midterm_range, final(midterm_range), 'b-', label='Prediction')
+    plt.xlabel('Midterm scores')
+    plt.ylabel('Final scores')
+    plt.xlim(midterm_range)
+    plt.ylim(final_range)
+    plt.grid()
+    plt.legend()
+    plt.show()
